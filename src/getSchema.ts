7654f24994bafb4eaa18e68931742c1db7bf810a
ccpu <m.doaie@hotmail.com>
2020-09-13 12:25:37 +0430

    fixed generateTypeKeys

--- src/getSchema.ts
+++ src/getSchema.ts
@@ -110,7 +110,7 @@ export const generateTypeKeys = (
     return;
   }
 
-  const schema = getSchemaObject(node, state, typeNames, options);
+  const schema = getSchemaObject(node, state, typeNames, { ...options, maxDepth: 1 });
 
   if (!schema.properties) {
     setNullValue(path, id);
